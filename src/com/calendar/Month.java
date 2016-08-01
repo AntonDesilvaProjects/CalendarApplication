7f80af3be1234b39e018665713e2f0924ceaa503
Anton Desilva <antondesilva09@yahoo.com>
2016-07-31 21:23:12 -0400

    updated the cal view

--- src/com/calendar/Month.java
+++ src/com/calendar/Month.java
@@ -9,7 +9,6 @@ public class Month {
 	private Day[][] days;
 	private YearMonth yearMonth;
 	
-	
 	private Month(int month, int year)
 	{
 		this.month = month;
@@ -17,11 +16,12 @@ public class Month {
 		yearMonth = YearMonth.of(year, month);
 		days = generateMonth(yearMonth);
 	}
-	
+	//Create a new month object
 	public static Month createMonth(int month, int year)
 	{
 		return new Month(month, year);
 	}
+	//File the 3D structure of the month with actual dates
 	private Day[][] generateMonth(YearMonth ym)
 	{
 		Day[][] monthDays = new Day[6][8];
@@ -49,17 +49,22 @@ public class Month {
 	}
 	public void printMonth()
 	{
+		System.out.println("Mon\tTue\tWed\tThur\tFri\tSat\tSun");
 		for(int i = 0; i < 6; i++)
 		{
 			for(int j = 1; j <= 7; j++)
 			{
 				if( days[i][j] != null)
-					System.out.print( days[i][j].getDate() + " | ");
+					System.out.print( days[i][j].getDate() + "\t");
 				else
-					System.out.print( " | ");
+					System.out.print( "\t");
 			}
 			System.out.println();
 		}
 			
+	}
+	public String generateHTMLView()
+	{
+		return new String();
 	}
 }
--- src/com/calendar/Runner.java
+++ src/com/calendar/Runner.java
@@ -1,11 +1,8 @@
 package com.calendar;
 
 public class Runner {
-
 	public static void main(String[] args) {
-		Month m = Month.createMonth(7, 2016);
-		m.printMonth();
-
+		Month.createMonth(2, 2012).printMonth();
 	}
 
 }
